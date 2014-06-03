ddddd0d48046b93e5ccd4e13ae3798b87160bce3
Joon Lee <l33j00n325@gmail.com>
2014-06-03 13:45:20 -0700

    Login Helper + Remove Logout Icon

--- src/com/example/bruinswipeswap/LogIn.java
+++ src/com/example/bruinswipeswap/LogIn.java
@@ -11,10 +11,12 @@ import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-
+import com.parse.GetCallback;
 import com.parse.LogInCallback;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.ParseException;
+import com.parse.RequestPasswordResetCallback;
 
 public class LogIn extends Activity {
  
@@ -26,7 +28,7 @@ public class LogIn extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 		addListenerOnLogInButton();
-		addListenerOnRegisterLink();		
+		addListenerOnHelpLink();
 	}
  
 	public void addListenerOnLogInButton() {
@@ -83,16 +85,81 @@ public class LogIn extends Activity {
 		});
 	}
 
-	public void addListenerOnRegisterLink() {
+	public void addListenerOnHelpLink() {
 		final Context context = this;
- 
-		view = (TextView) findViewById(R.id.login_textView_register);
+		
+		view = (TextView) findViewById(R.id.login_textView_help);
 		view.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View arg0) {
-			    Intent intent = new Intent(context, Register.class);
-                startActivity(intent);   
-			}
+			public void onClick(View arg0) {		
+				AlertDialog.Builder m_builder = new AlertDialog.Builder(context);
+
+				String options_array[] = {  "Register for an Account",
+											"Forgot Password", };
+        
+				m_builder.setTitle("Login Helper")
+					.setItems(options_array, new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							switch(which)
+							{
+							case 0:
+								Intent intent = new Intent(context, Register.class);
+								startActivity(intent); 
+								break;
+							case 1:
+								TextView tv_email_query = (TextView) findViewById(R.id.login_editText_email);
+								final String s_email_query = tv_email_query.getText().toString();
+        				
+								ParseQuery<ParseUser> query = ParseUser.getQuery();
+								query.whereEqualTo("email", s_email_query);
+								query.getFirstInBackground(new GetCallback<ParseUser>() {
+									public void done(ParseUser user, ParseException e) {
+										if (e == null) {
+											ParseUser.requestPasswordResetInBackground(s_email_query,
+													new RequestPasswordResetCallback() {
+												public void done(ParseException e) {
+													if (e == null) {
+														AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+														builder.setTitle("Reset Password")
+															.setMessage("An email has been sent with instructions on resetting your password.");
+        			    			                
+														builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+        			    			                	
+															public void onClick(DialogInterface dialog, int id){}
+														});
+
+														AlertDialog dialog = builder.create();
+
+														dialog.show();
+													}
+												}
+											});
+										} 
+										else {
+											AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+											builder.setTitle("Email Not Found").setMessage("Check your email and try again.");
+        			                
+											builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+												public void onClick(DialogInterface dialog, int id){}
+											});
+
+											AlertDialog dialog = builder.create();
+
+											dialog.show();
+										}
+									}
+								});
+								break;
+							}
+						}
+					});
+								
+                AlertDialog dialog = m_builder.create();
+
+                dialog.show();
+            }
 		});
 	}
 }
