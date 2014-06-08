2c6dba72e799ae01b241634cf5dccfed352d7436
Joon Lee <l33j00n325@gmail.com>
2014-06-08 02:52:01 -0700

    Checks for Duplicate Emails #2
    Fixed priority of Error messages

--- src/com/example/bruinswipeswap/Register.java
+++ src/com/example/bruinswipeswap/Register.java
@@ -53,78 +53,79 @@ public class Register extends Activity {
             	
             	final String regex_number = "^[0-9]{10}$";
 
-            	ParseQuery<ParseUser> query = ParseUser.getQuery();
-            	query.whereEqualTo("username", s_email);
-            	query.findInBackground(new FindCallback<ParseUser>() {
-            		public void done (List<ParseUser> objects, ParseException e) {
-            			if (e == null)
-            			{
-                			AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        					
-                			builder.setTitle("Account Already Exists")
-                				.setMessage("This email already exists in our database.");
-        					
-        					builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
-        					
-        						public void onClick(DialogInterface dialog, int id){}
-        					});
+        		// Check if there is a name
+        		if (s_name.isEmpty())
+        		{
+        			AlertDialog.Builder builder = new AlertDialog.Builder(context);
+					
+        			builder.setTitle("No Name").setMessage("Please enter a Name");
+					
+					builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+					
+						public void onClick(DialogInterface dialog, int id){}
+					});
+			
+					AlertDialog dialog = builder.create();
+			
+					dialog.show();        			
+        		}
+    				// Check if phone number if proper format
+        		else if (!s_number.matches(regex_number))
+        		{
+        			AlertDialog.Builder builder = new AlertDialog.Builder(context);
         			
-        					AlertDialog dialog = builder.create();
+        			builder.setTitle("Invalid Phone Number").setMessage("XXXXXXXXXX Format Only");
         			
-        					dialog.show();  
-            			}
-            			else
-            			{
-                    		// Check if there is a name
-                    		if (s_name.isEmpty())
-                    		{
-                    			AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            					
-                    			builder.setTitle("No Name").setMessage("Please enter a Name");
-            					
-            					builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
-            					
-            						public void onClick(DialogInterface dialog, int id){}
-            					});
-            			
-            					AlertDialog dialog = builder.create();
-            			
-            					dialog.show();        			
-                    		}
-                    			// Check if phone number if proper format
-                    		else if (!s_number.matches(regex_number))
-                    		{
-                    			AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            					
-                    			builder.setTitle("Invalid Phone Number").setMessage("XXXXXXXXXX Format Only");
-            					
-            					builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
-            					
-            						public void onClick(DialogInterface dialog, int id){}
-            					});
-            			
-            					AlertDialog dialog = builder.create();
-            			
-            					dialog.show();
-                    		}
-                    			// Check if UCLA email
-                    		else if (!(s_email.endsWith("@ucla.edu") || s_email.endsWith("@g.ucla.edu")))
-                    		{
-                    			AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            					
-                    			builder.setTitle("Invalid Email Address").setMessage("Please enter a correct UCLA Email address");
-            					
-            					builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
-            					
-            						public void onClick(DialogInterface dialog, int id){}
-            					});
-            			
-            					AlertDialog dialog = builder.create();
-            			
-            					dialog.show();
-                    		}
-                    		else {
-                    				// Check if password is confirmed correctly
+        			builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+				
+        				public void onClick(DialogInterface dialog, int id){}
+        			});
+		
+        			AlertDialog dialog = builder.create();
+		
+        			dialog.show();
+        		}
+        			// Check if UCLA email
+        		else if (!(s_email.endsWith("@ucla.edu") || s_email.endsWith("@g.ucla.edu")))
+        		{
+        			AlertDialog.Builder builder = new AlertDialog.Builder(context);
+				
+        			builder.setTitle("Invalid Email Address").setMessage("Please enter a correct UCLA Email address");
+				
+        			builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+				
+        				public void onClick(DialogInterface dialog, int id){}
+        			});
+		
+        			AlertDialog dialog = builder.create();
+		
+        			dialog.show();
+        		}
+        		else
+        		{
+        			ParseQuery<ParseUser> query = ParseUser.getQuery();
+        			query.whereEqualTo("username", s_email);
+        			query.findInBackground(new FindCallback<ParseUser>() {
+        				public void done (List<ParseUser> objects, ParseException e) {
+        					if (e == null)
+        					{
+        						AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        						
+        						builder.setTitle("Account Already Exists")
+        							.setMessage("This email already exists in our database.");
+        					
+        						builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+        					
+        							public void onClick(DialogInterface dialog, int id){}
+        						});
+        			
+        						AlertDialog dialog = builder.create();
+        			
+        						dialog.show();  
+        					}
+        					else
+        					{	
+        							// Check if password is confirmed correctly
                     			if (s_password.equals(s_password2) && !s_password.isEmpty()){ 
                     				ParseUser new_user = new ParseUser();
                     				new_user.setUsername(s_email);
@@ -174,12 +175,10 @@ public class Register extends Activity {
                     				dialog.show();
                     			}
                     		}
-
             			}
-            		}
-            	});
+            		});
+            	}
             }
-
         });
 
         Button cancel_button = (Button) findViewById(R.id.register_cancel_button);
